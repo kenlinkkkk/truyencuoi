b875a31b3eec653233210c80ea759237808f3779
NTT <tungnt15ptit@gmail.com>
2020-03-20 18:01:50 +0700

    asdasjk

--- resources/views/home/layout/footer.blade.php
+++ resources/views/home/layout/footer.blade.php
@@ -1,1 +1,22 @@
-<?php
+<div class="container p-3">
+    <div class="row">
+        <div class="col-3">
+            <a href="#"><img class="" src="{{ asset('assets/main/images/logo4-dark.png') }}" alt="" width="150"></a>
+        </div>
+        <div class="col-9">
+            <h4>CÔNG TY CỔ PHẦN CÔNG NGHỆ VÀ GIẢI PHÁP TRUYỀN THÔNG SỐ TMT</h4>
+            <ul>
+                <li>Số đăng ký kinh doanh: 0108741606 cấp ngày 15/05/2019 tại Phòng đăng ký kinh doanh, Sở kế hoạch và Đầu tư Hà Nội</li>
+                <li>Thông tin thiết lập trang tin điện tử tổng hợp số 3267/GP-TTĐT do Sở thông tin và truyền thông thành phố Hà Nội cấp ngày 28 tháng 10 năm 2015.</li>
+                <li>Địa Chỉ: Số 149 Lê Văn Hiến, Tổ 6, Phường Đức Thắng, Quận Bắc Từ Liêm, Thành phố Hà Nội, Việt Nam</li>
+                <li>Người đại diện pháp luật: Đào Giang Nam</li>
+            </ul>
+            <p>Điện thoại: 0904 689 899</p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid"  style="background-color: #f0f0f0;">
+    <div class="container">
+        <p>© 2019 All Rights Reserved</p>
+    </div>
+</div>
