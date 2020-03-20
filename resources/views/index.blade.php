8d0e56107c17b913b15dffe9b5af942e1684b2cf
NTT <tungnt15ptit@gmail.com>
2020-03-20 16:18:05 +0700

    asd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,10 @@
 @section('content')
     @foreach($posts as $post)
         <div class="row p-2">
-            <div class="col-4">
+            <div class="col-5">
                 <img src="{{ asset($post->picture) }}" class="img-fluid" style="max-width: 90px">
             </div>
-            <div class="col-8">
+            <div class="col-7">
                 <h4>{{ $post->name }}</h4>
                 <div style="line-height: 1.6em; height: 3em; overflow: hidden; white-space: nowrap; text-overflow: ellipsis; width: 100%;">
                     {!! $post->content !!}
